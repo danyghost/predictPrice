17dd3d232b2463e99ad1bd39cd58a66c759ddb4f
danyghost <danyghostnew@gmail.com>
2026-06-12 09:31:11 +0300

    Удалил не нужный скрипт

--- rec_model.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-from catboost import CatBoostRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-
-data = pd.read_csv('reccom/movies_data.csv')
